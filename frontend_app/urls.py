4bd40daef32e763ae4db26406e1cc39437d0673b
Hubertrich <hubertrichardo1703@gmail.com>
2026-04-14 11:53:21 +0800

    Finished creating main assets page including its component select_asset and upload maintenance_log

--- frontend_app/urls.py
+++ frontend_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path("assets/", views.assets, name='assets'),
+    path("select_asset/", views.select_asset, name='select_asset'),
+    path("maintenance_log/", views.maintenance_log, name='maintenance_log'),
+
+
 ]
--- frontend_app/views.py
+++ frontend_app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'frontend/dashboard.html')
+
+def assets(request):
+    return render(request, 'frontend/assets.html')
+
+def select_asset(request):
+    return render(request, 'frontend/select_asset.html')
+
+def maintenance_log(request):
+    return render(request, 'frontend/maintenance_log.html')
