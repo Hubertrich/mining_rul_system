d24fb5c65f3ebdda628d221d67570b0edf2b1d9b
Hubertrich <hubertrichardo1703@gmail.com>
2026-04-15 11:47:50 +0800

    Created 3 pages under the main asset page, Standardized sidebar layout and Connected inter-page navigation

--- frontend_app/urls.py
+++ frontend_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "frontend"
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path("assets/", views.assets, name='assets'),
     path("select_asset/", views.select_asset, name='select_asset'),
-    path("maintenance_log/", views.maintenance_log, name='maintenance_log'),
-
-
+    path("maintenance_logs/", views.maintenance_log, name='maintenance_logs'),
 ]
